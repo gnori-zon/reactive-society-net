982b161e310c76d8c137d6cb2a52819da80e0ff2
gnori-zon <konasenkos@gmail.com>
2024-02-25 19:36:25 +0300

    #1 #add #global - init

---
+++ src/main/java/org/gnori/reactivesocietynet/ReactiveSocietyNetApplication.java
@@ -0,0 +1,12 @@
+package org.gnori.reactivesocietynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSocietyNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSocietyNetApplication.class, args);
+    }
+}
---
+++ src/test/java/org/gnori/reactivesocietynet/ReactiveSocietyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gnori.reactivesocietynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSocietyNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
